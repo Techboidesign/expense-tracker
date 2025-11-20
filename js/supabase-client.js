449371699556cbaf0243f275f95846de02445f74
Alejandro Alvarez <35201107+Techboidesign@users.noreply.github.com>
2025-11-20 16:00:16 +0100

    Updated package.json

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_SUPABASE_ANON_KEY;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 let supabaseClient = null;
 let currentUser = null;
